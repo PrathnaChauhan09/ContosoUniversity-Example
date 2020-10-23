bc80d3ee9f5056ae356c46b742715461d9d1c8c0
PrathnaChauhan09 <prathna@promactinfo.com>
2020-10-23 12:50:42 +0530

    adding files

---
+++ Models/Instructor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.Models
+{
+    public class Instructor : Person
+    {
+        
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString ="{0:yyyy-MM-dd}",ApplyFormatInEditMode =true)]
+        [DisplayName("Hire Date")]
+        public DateTime HireDate { get; set; }
+
+
+        public ICollection<CourseAssignment> CourseAssignments { get; set; }
+        public OfficeAssignment OfficeAssignment { get; set; }
+    }
+}
